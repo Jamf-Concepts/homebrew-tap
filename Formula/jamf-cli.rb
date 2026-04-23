5d7b5f0573692061c77dc27ae6e3e861d0c7fc80
Neil Martin <neil.martin@jamf.com>
2026-04-23 14:41:51 +0100

    chore: bump jamf-cli formula to v1.13.0

--- Formula/jamf-cli.rb
+++ Formula/jamf-cli.rb
@@ -6,22 +6,22 @@
 class JamfCli < Formula
   desc "CLI for the Jamf platform"
   homepage "https://github.com/Jamf-Concepts/jamf-cli"
-  version "1.12.0"
+  version "1.13.0"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/Jamf-Concepts/jamf-cli/releases/download/v#{version}/jamf-cli-#{version}-darwin-universal.tar.gz"
-    sha256 "588bb6e4efb8f3ad8bb40130f7e940e435aae0dd0b19c1c1fc191df74b32dd73"
+    sha256 "207a8ea606e92179633445ca6bba85fa7a4dd9998c2e5f2b916e032148ed6b70"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Jamf-Concepts/jamf-cli/releases/download/v#{version}/jamf-cli-#{version}-linux-amd64.tar.gz"
-    sha256 "7ba0960ed62b913e09bf5f2b073cdff6a3b10eca81a6f0853133a4141fbcf093"
+    sha256 "365256aca4b4b41995ca7c954c2f9aed36f89d5fc6f1ebe9f61261ff5ae0d01b"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/Jamf-Concepts/jamf-cli/releases/download/v#{version}/jamf-cli-#{version}-linux-arm64.tar.gz"
-    sha256 "e0fb519992989f01769faf15e9de9511192a2e4b35ecce006d6bcd5c02e693ba"
+    sha256 "dcefdcb55f849d29fbca3509fc6f7c8850c8251fb3debdf90ff906698f8a83cd"
   end
 
   def install
