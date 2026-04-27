524f431b542b6d7b929880619d9b7447adf5273f
Neil Martin <neil.martin@jamf.com>
2026-04-27 12:36:34 +0100

    chore: update license to MIT and refactor URL definitions in jamformer formula

--- Formula/jamformer.rb
+++ Formula/jamformer.rb
@@ -7,23 +7,21 @@ class Jamformer < Formula
   desc "Export your Jamf configuration as Terraform HCL"
   homepage "https://github.com/Jamf-Concepts/jamformer"
   version "0.1.0"
-  license "LicenseRef-Jamf-Source-Available"
+  license "MIT"
 
-  on_macos do
-    url "https://github.com/Jamf-Concepts/jamformer/releases/download/v0.1.0/jamformer-0.1.0-darwin-universal.tar.gz"
+  if OS.mac?
+    url "https://github.com/Jamf-Concepts/jamformer/releases/download/v#{version}/jamformer-#{version}-darwin-universal.tar.gz"
     sha256 "f2066f3685d7257f14e59030aa12d3a14e13638f87c9d530c33a9d7da25e7685"
   end
 
-  on_linux do
-    on_intel do
-      url "https://github.com/Jamf-Concepts/jamformer/releases/download/v0.1.0/jamformer-0.1.0-linux-amd64.tar.gz"
-      sha256 "e955c4098ce8c39e79fbcdcda80f14ef8655102e44a049c70307d57c74a30259"
-    end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/Jamf-Concepts/jamformer/releases/download/v#{version}/jamformer-#{version}-linux-amd64.tar.gz"
+    sha256 "e955c4098ce8c39e79fbcdcda80f14ef8655102e44a049c70307d57c74a30259"
+  end
 
-    on_arm do
-      url "https://github.com/Jamf-Concepts/jamformer/releases/download/v0.1.0/jamformer-0.1.0-linux-arm64.tar.gz"
-      sha256 "9a628c5318e598dcdbf70c3267542182354aa0a53b82c0d0adc44390f45580ec"
-    end
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/Jamf-Concepts/jamformer/releases/download/v#{version}/jamformer-#{version}-linux-arm64.tar.gz"
+    sha256 "9a628c5318e598dcdbf70c3267542182354aa0a53b82c0d0adc44390f45580ec"
   end
 
   def install
